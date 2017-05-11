35965e2eb6b62b250202bae6df14c6d50054c208
somename <someemail>
2017-05-11 20:18:16 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -8,5 +8,16 @@ public class TestBowling {
 	public void test() {
 		fail("Not yet implemented");
 	}
+	
+	
+	@Test
+	public void testFrameIsStrike() throws BowlingException {
+		Frame frejm = new Frame(10,0);
+		
+		boolean strike = frejm.isStrike();
+		
+		assertEquals("strike", strike);
+	}
+	
 
 }
